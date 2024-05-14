b48b7587dcfc08d69368ba7817f6b2150fe7deb9
fireFistAniket <sahaaniket720@gmail.com>
2024-05-14 08:19:27 +0530

    base ready

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+function useFetch(url, body = null) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // Only proceed if url is not null
+    if (!url) return;
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const options = {
+          method: body ? "POST" : "GET",
+          headers: {
+            "Content-Type": "application/json",
+            // Additional headers can go here if needed
+          },
+          body: body ? JSON.stringify({ query: body }) : null,
+        };
+
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URI}${url}`,
+          options
+        );
+        const responseData = await response.json();
+
+        setData(responseData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, body]);
+
+  return { data, error, loading };
+}
+
+export default useFetch;
